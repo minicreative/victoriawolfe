6d7411efb11ff22ba0a2b669c557b09c582df568
Tomas Roy <hello@minicreative.net>
2024-07-11 15:42:24 -0500

    Initial commit: Jekyll setup, Dockerfile

---
+++ index.md
@@ -0,0 +1,7 @@
+---
+title: Home
+permalink: "/"
+layout: page
+---
+
+Victoria Wolfe: Hello world
---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// On Page Load
+$(document).ready(function() {
+})
+
+// On Stricter Page Load
+$(window).on('load', function () {
+})
+
+// On Page Resize
+$(window).resize(function () {
+})
